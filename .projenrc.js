d7332ce0916273df9b236c379ebee7748c38e21c
awlsring <mattcanemail@gmail.com>
2023-01-11 21:24:50 -0800

    chore: bump version

--- .projenrc.js
+++ .projenrc.js
@@ -2,16 +2,14 @@ const { cdktf } = require('projen');
 const project = new cdktf.ConstructLibraryCdktf({
   author: 'awlsring',
   authorAddress: 'mattcanemail@gmail.com',
-  cdktfVersion: '^0.13.0',
+  cdktfVersion: '^0.13.2',
+  constructsVersion: '^10.1.52',
   defaultReleaseBranch: 'main',
   name: 'cdktf-surreal-backend',
   repositoryUrl: 'https://github.com/awlsring/cdktf-surreal-backend.git',
   description: 'A package that vends a construct to setup the surreal backend in CDKTF',
   packageName: '@awlsring/cdktf-surreal-backend',
   releaseToNpm: true,
-  bundledDependencies: [
-    'cdktf@^0.13.0',
-  ],
   keywords: [
     'cdktf',
     'surrealdb',
