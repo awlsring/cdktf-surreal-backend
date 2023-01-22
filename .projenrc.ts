ed0528b7562d1abbc8dab12759d150029f2c6ee9
awlsring <mattcanemail@gmail.com>
2023-01-22 14:08:09 -0800

    fix: remove go

--- .projenrc.ts
+++ .projenrc.ts
@@ -3,6 +3,7 @@ import { AwlsringCdktfLibrary } from '@awlsring/projen-commons';
 const project = new AwlsringCdktfLibrary({
   cdktfVersion: '^0.14.3',
   constructsVersion: '^10.1.52',
+  majorVersion: 1,
   name: 'cdktf-surreal-backend',
   repositoryUrl: 'https://github.com/awlsring/cdktf-surreal-backend.git',
   description: 'A package that vends a construct to setup the surreal backend in CDKTF',
--- awlsringcdktfsurrealbackend/go.mod
+++
@@ -1,9 +0,0 @@
-module github.com/awlsring/cdktf-surreal-backend/awlsringcdktfsurrealbackend
-
-go 1.18
-
-require (
-	github.com/aws/jsii-runtime-go v1.73.0
-	github.com/hashicorp/terraform-cdk-go/cdktf v0.14.3
-	github.com/aws/constructs-go/constructs/v10 v10.1.227
-)
